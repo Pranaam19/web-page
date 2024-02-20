be7553b32e3a20159f3bb6a9b757664c1faaed48
Pranaam19 <22j34.pranam@sjec.ac.in>
2024-02-20 12:27:38 +0530

    added middleware

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,12 +8,12 @@ export default function Header() {
         <a
           href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
           target="_blank"
-          rel="noreferrer"
+          rel="noopener"
         >
           <SupabaseLogo />
         </a>
         <span className="border-l rotate-45 h-6" />
-        <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
+        <a href="https://nextjs.org/" target="_blank" rel="noopener">
           <NextLogo />
         </a>
       </div>
@@ -24,7 +24,8 @@ export default function Header() {
           href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
           target="_blank"
           className="font-bold hover:underline"
-          rel="noreferrer"
+          rel="noopener"
+          title="Supabase"
         >
           Supabase
         </a>{" "}
@@ -33,7 +34,8 @@ export default function Header() {
           href="https://nextjs.org/"
           target="_blank"
           className="font-bold hover:underline"
-          rel="noreferrer"
+          rel="noopener"
+          title="Next.js"
         >
           Next.js
         </a>
